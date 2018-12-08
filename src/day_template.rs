7a3c280419bac4657416b009093989bdf2932dec
Felix Kohlgrüber <felix.kohlgrueber@gmail.com>
2018-12-08 14:11:50 +0100

    add day template

---
+++ src/day_template.rs
@@ -0,0 +1,28 @@
+
+
+pub fn calc(input: &str) -> (i32, i32) {
+    (part_1(input), part_2(input))
+}
+
+fn part_1(input: &str) -> i32 {
+    unimplemented!()
+}
+
+fn part_2(input: &str) -> i32 {
+    unimplemented!()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_part_1() {
+        assert_eq!(part_1(""), 0);
+    }
+
+    #[test]
+    fn test_part_2() {
+        assert_eq!(part_2(""), 0);
+    }
+}
