3dc371a7e7daa57e0538a6f73194e59f4abda011
Felix Kohlgrüber <felix.kohlgrueber@gmail.com>
2018-12-08 21:24:53 +0100

    day 4

---
+++ src/day4.rs
@@ -0,0 +1,155 @@
+extern crate regex;
+extern crate chrono;
+
+use regex::Regex;
+use std::collections::HashMap;
+use chrono::prelude::*;
+
+
+pub fn calc(input: &str) -> (i32, i32) {
+    (part_1(input), part_2(input))
+}
+
+fn part_1(input: &str) -> i32 {
+    let hash_map = calc_sleep_hist_per_guard(input);
+
+    // find id with most sleeping minutes
+    let mut max_sum = 0;
+    let mut max_id = 0;
+    for (id, v) in hash_map.iter(){
+        let sum_minutes: i32 = v.iter().sum();
+        if sum_minutes > max_sum{
+            max_sum = sum_minutes;
+            max_id = *id;
+        }
+    }
+
+    let res = hash_map[&max_id].iter().enumerate().max_by_key(|x| x.1).unwrap();
+
+    max_id * res.0 as i32
+}
+
+fn part_2(input: &str) -> i32 {
+    let hash_map = calc_sleep_hist_per_guard(input);
+
+    let res = hash_map.iter().map(get_max).max_by_key(|x| x.2).unwrap();
+
+    res.0 * res.1
+}
+
+fn calc_sleep_hist_per_guard(input : &str) -> HashMap<i32, [i32; 60]> {
+    
+    let mut events: Vec<Event> = input.lines().map(|x| Event::from_str(&x)).collect();
+    events.sort_by(|a, b| a.date_time.cmp(&b.date_time));
+
+    let mut hash_map : HashMap<i32, [i32; 60]> = HashMap::new();
+    let mut id = 0;
+    let mut asleep_min = 0;
+    for evt in events{
+        match evt {
+            Event{ evt_type: EventType::BeginsShift(i), ..} => id = i,
+            Event{ date_time: dt, evt_type: EventType::FallsAsleep} => asleep_min = dt.time().minute(),
+            Event{ date_time: dt, evt_type: EventType::WakesUp} => {
+                let entry = hash_map.entry(id).or_insert([0; 60]);
+                for m in asleep_min..dt.time().minute(){
+                    entry[m as usize] += 1;
+                }
+            }
+        }
+    }
+    hash_map
+}
+
+fn get_max(elmt: (&i32, &[i32; 60])) -> (i32, i32, i32){
+    let (k, v) = elmt;
+
+    let (min, freq) = v.iter().enumerate().max_by_key(|x| x.1).unwrap();
+
+    (*k, min as i32, *freq as i32)
+}
+
+#[derive(Debug)]
+enum EventType {
+    FallsAsleep,
+    WakesUp,
+    BeginsShift(i32)
+}
+
+#[derive(Debug)]
+struct Event {
+    date_time: DateTime<Utc>,
+    evt_type: EventType
+}
+
+impl Event {
+    fn from_str(s: &str) -> Event{
+        let re = Regex::new(r"\[(.+)\] (.+)").unwrap();
+        let cap = re.captures(s).unwrap();
+        Event{
+            date_time:Utc.datetime_from_str(&cap[1], "%Y-%m-%d %H:%M").unwrap(),
+            evt_type: match &cap[2]{
+                "falls asleep" => EventType::FallsAsleep,
+                "wakes up" => EventType::WakesUp,
+                s => {
+                    let reg = Regex::new(r"\d+").unwrap();
+                    let match_ = reg.find(s).unwrap();
+                    EventType::BeginsShift(match_.as_str().parse().unwrap())
+                }
+            },
+        }
+    }
+}
+
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_part_1() {
+        assert_eq!(
+            part_1("[1518-11-01 00:00] Guard #10 begins shift\n\
+                    [1518-11-01 00:05] falls asleep\n\
+                    [1518-11-01 00:25] wakes up\n\
+                    [1518-11-01 00:30] falls asleep\n\
+                    [1518-11-01 00:55] wakes up\n\
+                    [1518-11-01 23:58] Guard #99 begins shift\n\
+                    [1518-11-02 00:40] falls asleep\n\
+                    [1518-11-02 00:50] wakes up\n\
+                    [1518-11-03 00:05] Guard #10 begins shift\n\
+                    [1518-11-03 00:24] falls asleep\n\
+                    [1518-11-03 00:29] wakes up\n\
+                    [1518-11-04 00:02] Guard #99 begins shift\n\
+                    [1518-11-04 00:36] falls asleep\n\
+                    [1518-11-04 00:46] wakes up\n\
+                    [1518-11-05 00:03] Guard #99 begins shift\n\
+                    [1518-11-05 00:45] falls asleep\n\
+                    [1518-11-05 00:55] wakes up"), 
+            240
+        );
+    }
+
+    #[test]
+    fn test_part_2() {
+        assert_eq!(
+            part_2("[1518-11-01 00:00] Guard #10 begins shift\n\
+                    [1518-11-01 00:05] falls asleep\n\
+                    [1518-11-01 00:25] wakes up\n\
+                    [1518-11-01 00:30] falls asleep\n\
+                    [1518-11-01 00:55] wakes up\n\
+                    [1518-11-01 23:58] Guard #99 begins shift\n\
+                    [1518-11-02 00:40] falls asleep\n\
+                    [1518-11-02 00:50] wakes up\n\
+                    [1518-11-03 00:05] Guard #10 begins shift\n\
+                    [1518-11-03 00:24] falls asleep\n\
+                    [1518-11-03 00:29] wakes up\n\
+                    [1518-11-04 00:02] Guard #99 begins shift\n\
+                    [1518-11-04 00:36] falls asleep\n\
+                    [1518-11-04 00:46] wakes up\n\
+                    [1518-11-05 00:03] Guard #99 begins shift\n\
+                    [1518-11-05 00:45] falls asleep\n\
+                    [1518-11-05 00:55] wakes up"),
+            4455
+        );
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,7 @@ use structopt::StructOpt;
 mod day1;
 mod day2;
 mod day3;
+mod day4;
 mod day8;
 
 pub fn read_input(day_number: i32) -> std::io::Result<String> {
@@ -25,6 +26,7 @@ fn main() {
         1 => println!("{:?}", day1::calc(&read_input(1).unwrap())),
         2 => println!("{:?}", day2::calc(&read_input(2).unwrap())),
         3 => println!("{:?}", day3::calc(&read_input(3).unwrap())),
+        4 => println!("{:?}", day4::calc(&read_input(4).unwrap())),
         8 => println!("{:?}", day8::calc(&read_input(8).unwrap())),
         1..=24 => println!("This day is not implemented yet."),
         _ => println!("Day has to between 1 and 24."),
