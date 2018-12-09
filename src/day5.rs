aae2859c4028058d1b3bedf2ce11614f057e68a7
Felix Kohlgrüber <felix.kohlgrueber@gmail.com>
2018-12-09 20:18:09 +0100

    use stack for part 2 as well

--- src/day5.rs
+++ src/day5.rs
@@ -7,6 +7,28 @@ pub fn calc(input: &str) -> (String, String) {
 fn part_1(input: &str) -> i32 {
     let contents : Vec<char> = input.chars().collect();
 
+    react(&contents)
+}
+
+fn part_2(input: &str) -> i32 {
+    let contents = input.to_owned();
+
+    let contents_chars : Vec<char> = contents.chars().collect();
+
+    let mut all_chars : HashSet<char> = HashSet::new();
+    all_chars.extend(contents_chars.iter().map(|x| x.to_ascii_lowercase()));
+
+    all_chars.iter().map(|c| {
+        let removed_chars = remove(&contents, *c);
+        react(&removed_chars)
+    }).min().unwrap() as i32
+}
+
+fn same_char(a: char, b: char) -> bool {
+    a != b && a.to_ascii_lowercase() == b.to_ascii_lowercase()
+}
+
+fn react(contents : &[char]) -> i32{
     let mut stack = Vec::new();
     stack.reserve(contents.len());
 
@@ -26,39 +48,6 @@ fn part_1(input: &str) -> i32 {
     }
 
     stack.len() as i32
-}
-
-fn part_2(input: &str) -> i32 {
-    let contents = input.to_owned();
-
-    let contents_chars : Vec<char> = contents.chars().collect();
-
-    let mut all_chars : HashSet<char> = HashSet::new();
-    all_chars.extend(contents_chars.iter().map(|x| x.to_ascii_lowercase()));
-
-    all_chars.iter().map(|c| {
-        let mut removed_chars = remove(&contents, *c);
-        reduce(&mut removed_chars);
-        removed_chars.len()
-    }).min().unwrap() as i32
-}
-
-fn same_char(a: char, b: char) -> bool {
-    a != b && a.to_ascii_lowercase() == b.to_ascii_lowercase()
-}
-
-fn reduce(s: &mut Vec<char>){
-    let mut i = 0;
-    while i < s.len()-1 {
-        
-        if same_char(s[i], s[i+1]){
-            s.remove(i);
-            s.remove(i);
-            i = match i {0 => 0, x => x-1};
-        }else {
-            i += 1;
-        }
-    }
 }
 
 fn remove(s: &str, c: char) -> Vec<char>{
