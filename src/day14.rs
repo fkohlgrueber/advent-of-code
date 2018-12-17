e2339e44a1bda189f2532d96855f30c09bef2829
Felix Kohlgrüber <felix.kohlgrueber@gmail.com>
2018-12-17 16:09:38 +0100

    improve day 14

--- src/day14.rs
+++ src/day14.rs
@@ -1,36 +1,7 @@
 
 
 pub fn calc(input: &str) -> (String, String) {
-    (part_1(input).to_string(), part_2(input).to_string())
-}
-
-fn part_1(input: &str) -> String {
     let val: usize = input.parse().unwrap();
-    
-    let mut scores : Vec<u8> = Vec::with_capacity(val + 10 + 1);
-    scores.push(3);
-    scores.push(7);
-
-    let mut idx1 = 0;
-    let mut idx2 = 1;
-    while scores.len() < val + 10 {
-        // push new values
-        let new_num = scores[idx1] + scores[idx2];
-        if new_num >= 10 {
-            scores.push(1);
-            scores.push(new_num - 10);
-        } else {
-            scores.push(new_num);
-        }
-        // update indices
-        idx1 = (idx1 + scores[idx1] as usize + 1) % scores.len();
-        idx2 = (idx2 + scores[idx2] as usize + 1) % scores.len();
-    }
-
-    scores.iter().skip(val).take(10).map(|x| x.to_string()).collect::<String>()
-}
-
-fn part_2(input: &str) -> i32 {
     let vals: Vec<u8> = input.chars().map(|x| x.to_digit(10).unwrap() as u8).collect();
     
     let mut scores : Vec<u8> = vec!();
@@ -39,29 +10,45 @@ fn part_2(input: &str) -> i32 {
 
     let mut idx1 = 0;
     let mut idx2 = 1;
-    loop {
+    let mut res_part_2 = 0;
+    while res_part_2 == 0 || scores.len() < val + 10 {
         // push new values
         let new_num = scores[idx1] + scores[idx2];
         if new_num >= 10 {
             scores.push(1);
             if scores.len() >= vals.len() && vals.iter().zip(scores.iter().skip(scores.len() - vals.len()).take(vals.len())).all(|(a, b)| a == b) {
-                return (scores.len() - vals.len()) as i32;
+                if res_part_2 == 0 {
+                    res_part_2 = scores.len() - vals.len();
+                }
+                if scores.len() >= val + 10 { break }
             }
             scores.push(new_num - 10);
             if scores.len() >= vals.len() && vals.iter().zip(scores.iter().skip(scores.len() - vals.len()).take(vals.len())).all(|(a, b)| a == b) {
-                return (scores.len() - vals.len()) as i32;
+                if res_part_2 == 0 {
+                    res_part_2 = scores.len() - vals.len();
+                }
+                if scores.len() >= val + 10 { break }
             }
         } else {
             scores.push(new_num);
             if scores.len() >= vals.len() && vals.iter().zip(scores.iter().skip(scores.len() - vals.len()).take(vals.len())).all(|(a, b)| a == b) {
-                return (scores.len() - vals.len()) as i32;
+                if res_part_2 == 0 {
+                    res_part_2 = scores.len() - vals.len();
+                }
+                if scores.len() >= val + 10 { break }
             }
         }
         // update indices
-        idx1 = (idx1 + scores[idx1] as usize + 1) % scores.len();
-        idx2 = (idx2 + scores[idx2] as usize + 1) % scores.len();
-    }
+        idx1 = idx1 + scores[idx1] as usize + 1;
+        idx2 = idx2 + scores[idx2] as usize + 1;
+        idx1 -= (idx1 >= scores.len()) as usize * scores.len();
+        idx2 -= (idx2 >= scores.len()) as usize * scores.len();
+        idx1 -= (idx1 >= scores.len()) as usize * scores.len();
+        idx2 -= (idx2 >= scores.len()) as usize * scores.len();
+    };
+    (scores.iter().skip(val).take(10).map(|x| x.to_string()).collect::<String>(), res_part_2.to_string())
 }
+
 
 #[cfg(test)]
 mod tests {
@@ -69,17 +56,17 @@ mod tests {
 
     #[test]
     fn test_part_1() {
-        assert_eq!(part_1("9"), "5158916779");
-        assert_eq!(part_1("5"), "0124515891");
-        assert_eq!(part_1("18"), "9251071085");
-        assert_eq!(part_1("2018"), "5941429882");
+        assert_eq!(calc("9").0, "5158916779");
+        assert_eq!(calc("5").0, "0124515891");
+        assert_eq!(calc("18").0, "9251071085");
+        assert_eq!(calc("2018").0, "5941429882");
     }
 
     #[test]
     fn test_part_2() {
-        assert_eq!(part_2("51589"), 9);
-        assert_eq!(part_2("01245"), 5);
-        assert_eq!(part_2("92510"), 18);
-        assert_eq!(part_2("59414"), 2018);
+        assert_eq!(calc("51589").1, "9");
+        assert_eq!(calc("01245").1, "5");
+        assert_eq!(calc("92510").1, "18");
+        assert_eq!(calc("59414").1, "2018");
     }
 }
