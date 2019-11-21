2d83145ba43cb222d96b3928d0642bcb5022f221
Felix Kohlgrüber <felix.kohlgrueber@gmail.com>
2019-11-21 18:10:56 +0100

    move 2018 input files

--- src/main.rs
+++ src/main.rs
@@ -50,13 +50,13 @@ fn get_func(day_number: i32) -> Result<CalcFunction, String> {
         20 => Ok(day20::calc),
         21 => Ok(day21::calc),
         22 => Ok(day22::calc),
-        n @ 1..=24 => Err(format!("Day {} is not implemented yet.", n)),
+        n @ 23..=24 => Err(format!("Day {} is not implemented yet.", n)),
         _ => Err("Day has to between 1 and 24.".to_owned()),
     }
 }
 
 pub fn read_input(day_number: i32) -> std::io::Result<String> {
-    let mut file = File::open(format!("inputs/input{}.txt", day_number))?;
+    let mut file = File::open(format!("inputs/year2018/input{:02}.txt", day_number))?;
     let mut contents = String::new();
     file.read_to_string(&mut contents)?;
     Ok(contents.trim().to_owned())
