b4d1503f9a42e7b1d828eaea641f223880af5d57
Felix Kohlgrüber <felix.kohlgrueber@gmail.com>
2019-11-21 14:09:33 +0100

    add aoc-workbench

---
+++ aoc-workbench/src/aoc_workbench.rs
@@ -0,0 +1,21 @@
+#[allow(unused_imports)]
+use aoc_tools::prelude::*;
+use crate::Challenge;
+
+pub struct Day();
+
+type Input = Vec<Vec<usize>>;
+
+impl Challenge<Input> for Day {
+    fn parse(input: String) -> Input {
+        input
+    }
+
+    fn part_1(input: Input) -> String {
+        "".to_string()
+    }
+    
+    fn part_2(input: Input) -> String {
+        "".to_string()
+    }
+}
---
+++ aoc-workbench/src/main.rs
@@ -0,0 +1,51 @@
+
+mod aoc_workbench;
+//mod test;
+
+
+pub trait Challenge<T> {
+    fn parse(input: String) -> T;
+
+    fn part_1(input: T) -> String;
+    
+    fn part_2(input: T) -> String;
+}
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let s = std::fs::read_to_string("input.txt")?;
+    let data = aoc_workbench::Day::parse(s);
+    /*
+    println!("Part 1 tests");
+    for (i, o) in aoc_workbench::Day::tests_part_1() {
+        let data = aoc_workbench::Day::parse(i.to_string());
+        assert_eq!(aoc_workbench::Day::part_1(data), o);
+    }*/
+    println!("Part 1:");
+    let res_1 = aoc_workbench::Day::part_1(data.clone());
+    println!("{}", res_1);
+    if !res_1.is_empty() {
+        std::fs::write("result1.txt", res_1)?;
+    }
+    println!("Part 2:");
+    let res_2 = aoc_workbench::Day::part_2(data);
+    println!("{}", res_2);
+    if !res_2.is_empty() {
+        std::fs::write("result2.txt", res_2)?;
+    }
+    Ok(())
+}
+
+
+/*fn main() -> Result<(), Box<dyn std::error::Error>> {
+    for elmt in test::description(2018, 3)? {
+        if let test::Elmt::Block(s) = elmt {
+            /*if is_valid_input(&s) {
+                println!("Valid input:\n{}\n\n", s)
+            }*/
+        }
+    }
+    
+    //test::get_input(2018, 3)?;
+    Ok(())
+}
+*/
